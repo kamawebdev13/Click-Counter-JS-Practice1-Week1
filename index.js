02ba65083a60bf86cb6fb34cacfc5083b80e038d
Karina Matos <karinadatanalyst13@alumnos.cei.es>
2025-10-25 13:00:55 +0000

    Add initial HTML, CSS, and JavaScript for click counter application

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+let counter = 0;
+
+function updateCounter() {
+    counter += 1;
+    document.querySelector("div p").innerText = `Counter: ${counter}:`;
+    
+}
+
+document.getElementById("increase").addEventListener("click", updateCounter);
+
+
+function resetCounter() {
+    counter = 0;
+    document.querySelector("div p").innerText = `Counter: ${counter}:`;
+}
+
+document.getElementById("reset").addEventListener("click", resetCounter);
